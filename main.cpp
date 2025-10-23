4cff96199027891711eb74dfe09610ed359ab58f
Remi-Ge <140516143+Remi-Ge@users.noreply.github.com>
2025-10-23 12:07:22 +0200

    Add Program Argument

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include "machine.cpp"
+#include <string>
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <program>" << std::endl;
+        return 1;
+    }
+    std::string programPath = argv[1];
     Machine machine = Machine();
+    machine.loadProgram(programPath);
     machine.run();
 }
